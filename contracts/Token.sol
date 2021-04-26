00768a8ad429bb828cc4475e0059612a879cb6b4
Solange Gueiros <solangegueiros@gmail.com>
2021-04-26 01:11:50 -0300

    2021

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -5,7 +5,7 @@ import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
 contract Token is ERC20  {
 
-    constructor(uint256 initialSupply) public ERC20("My token", "MTO") {
+    constructor(uint256 initialSupply) ERC20("My token", "MTO") {
         _mint(msg.sender, initialSupply);
     }
 
