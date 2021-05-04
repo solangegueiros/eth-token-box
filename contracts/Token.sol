55087864aa4e3ad7e667e56fc06458cd7369466e
Solange Gueiros <solangegueiros@gmail.com>
2021-05-04 20:21:39 -0300

    ny bootcamp

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,15 +1,7 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.4;
-
-import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
-
-contract Token is ERC20  {
-
-    constructor(uint256 initialSupply) ERC20("My token", "MTO") {
-        _mint(msg.sender, initialSupply);
-    }
-
-    function decimals() public pure override returns (uint8) {
-        return 2;
-    }
+pragma solidity 0.5.2;
+import '@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol';
+contract Token is ERC20Mintable{
+       string public name = "My RSK token";
+       string public symbol = "MRT";
+       uint8 public decimals = 2;
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,5 @@
 var Token = artifacts.require("Token");
 
-const decimals = 2;
-const initialSupply = 100 * 10 ** decimals;
-
-module.exports = async (deployer, network, accounts) => {
-  await deployer.deploy(Token, initialSupply);
-  token = await Token.deployed();
-  console.log("token.address", token.address, "\n\n");  
+module.exports = function(deployer) {
+  deployer.deploy(Token);
 };
